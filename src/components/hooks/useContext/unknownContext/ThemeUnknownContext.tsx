b361ee097f56dc715c551af241425656f118fdd6
peterlidee <peter@lidee.be>
2023-06-12 12:04:33 +0200

    Updated without type assertion

--- src/components/hooks/useContext/unknownContext/ThemeUnknownContext.tsx
+++ src/components/hooks/useContext/unknownContext/ThemeUnknownContext.tsx
@@ -1,23 +1,27 @@
-import { createContext, useState } from "react";
-
+import { createContext, useState } from 'react';
 
 type Props = {
   children: React.ReactNode;
-}
+};
 type ThemeType = 'dark' | 'light';
 type ThemeUnknownContextType = {
   theme: ThemeType;
   toggleTheme: () => void;
-}
+};
 
-export const ThemeUnknownContext = createContext<ThemeUnknownContextType>({} as ThemeUnknownContextType);
-// or export const UnknownContext = createContext<null | ThemeUnknownContextType>(null);
+// export const ThemeUnknownContext = createContext<ThemeUnknownContextType>({} as ThemeUnknownContextType);
+export const ThemeUnknownContext =
+  createContext<null | ThemeUnknownContextType>(null);
 // also add conditionals when use useContext
 
 export const ThemeUnknownContextProvider = ({ children }: Props) => {
-  const [ theme, setTheme ] = useState<ThemeType>('dark');
+  const [theme, setTheme] = useState<ThemeType>('dark');
   const toggleTheme = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark');
-  }
-  return <ThemeUnknownContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeUnknownContext.Provider>
-}
+  };
+  return (
+    <ThemeUnknownContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeUnknownContext.Provider>
+  );
+};
--- src/components/hooks/useContext/unknownContext/UnknowContext.tsx
+++ src/components/hooks/useContext/unknownContext/UnknowContext.tsx
@@ -1,13 +1,22 @@
-import { useContext } from "react";
+import { useContext } from 'react';
 import { ThemeUnknownContext } from './ThemeUnknownContext';
 
-export default function UnknownContext(){
-  const { theme, toggleTheme } = useContext(ThemeUnknownContext);
-  return(
-    <div style={theme === 'dark' ? { background: '#333', color: '#fff' } : { background: "lightgreen", color: '#000' }}>
+export default function UnknownContext() {
+  const themeContext = useContext(ThemeUnknownContext);
+  const theme = themeContext?.theme;
+  const toggleTheme = themeContext?.toggleTheme;
+
+  return (
+    <div
+      style={
+        theme === 'dark'
+          ? { background: '#333', color: '#fff' }
+          : { background: 'lightgreen', color: '#000' }
+      }
+    >
       <strong>Unknown context</strong>
       <span>theme: {theme} </span>
       <button onClick={toggleTheme}>toggle theme</button>
     </div>
-  )
+  );
 }
